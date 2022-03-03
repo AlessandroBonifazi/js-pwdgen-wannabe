c7d638cadb16ff52c3a2558768fb4f58d97801cd
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-03 16:16:26 +0100

    adding js var

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // Control
-console.log('JS OK!');
+// console.log('JS OK!');
+
+// Form
+let pswOutput = document.getElementById('pswOutput');
+
+// Info
+let userName = prompt('Your name');
+
+let userSurname = prompt('Your Surname');
+
+let userColor = prompt('Your favourite color');
+
+let password = `${userName}${userSurname}${userColor}21`;
+
+document.getElementById('pswOutput').innerText = password;
